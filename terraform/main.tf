4c1338ac003741efaa80072847f43aa79e5a434d
Mark <17260591+MarkKleinhaus@users.noreply.github.com>
2024-11-25 08:18:18 -0800

    update backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  backend "s3" {
+    bucket         = "flite-terraform-state"
+    key            = "./terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock-table"
+    encrypt        = true
+  }
+}
+
+
 provider "aws" {
   region = "us-east-1"
 }
